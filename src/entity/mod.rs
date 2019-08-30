0ac344be0962faf4dae7fc7591e941a445ab9e12
konrad-c <ko.cybulski@gmail.com>
2019-08-30 13:05:16 +1000

    Refactored entity placeholder parsing to methods for complex types

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -47,15 +47,9 @@ impl Entity {
     pub fn value_of(&mut self, placeholder: Placeholder) -> Result<String, PlaceholderParseError> {
         println!("{}, {:?}", placeholder.data_type, placeholder.args);
         match placeholder.data_type.as_str() {
-            "name::first" => Ok(self.name.first()),
-            "name::last" => Ok(self.name.last()),
-            "name::full" => Ok(self.name.full()),
-            "phone" => Ok(self.phone.phone()),
-            "phone::mobile" => Ok(self.phone.mobile()),
-            "phone::landline" => Ok(self.phone.landline()),
-            "location::place" => Ok(self.location.place()),
-            "location::street" => Ok(self.location.street()),
-            "location::address" => Ok(self.location.address()),
+            name_datatype if name_datatype.starts_with("name") => self.name_placeholder(&placeholder),
+            phone_datatype if phone_datatype.starts_with("phone") => self.phone_placeholder(&placeholder),
+            location_datatype if location_datatype.starts_with("location") => self.location_placeholder(&placeholder),
             "float" => placeholder.args.clone()
                 .and_then(|args:String| FloatArgs::parse(&args))
                 .map(|args: FloatArgs| self.float.get(args).to_string())
@@ -69,6 +63,33 @@ impl Entity {
                 .and_then(|args:String| SetArgs::parse(&args))
                 .map(|args: SetArgs| self.set.get(args))
                 .ok_or(PlaceholderParseError::invalid_arg(&placeholder, SetArgs::help())),
+            _ => Err(PlaceholderParseError::invalid_placeholder(placeholder.to_string().as_str()))
+        }
+    }
+
+    fn name_placeholder(&mut self, placeholder: &Placeholder) -> Result<String, PlaceholderParseError> {
+        match placeholder.data_type.as_str() {
+            "name::first" => Ok(self.name.first()),
+            "name::last" => Ok(self.name.last()),
+            "name::full" => Ok(self.name.full()),
+            _ => Err(PlaceholderParseError::invalid_placeholder(placeholder.to_string().as_str()))
+        }
+    }
+
+    fn phone_placeholder(&mut self, placeholder: &Placeholder) -> Result<String, PlaceholderParseError> {
+        match placeholder.data_type.as_str() {
+            "phone" => Ok(self.phone.phone()),
+            "phone::mobile" => Ok(self.phone.mobile()),
+            "phone::landline" => Ok(self.phone.landline()),
+            _ => Err(PlaceholderParseError::invalid_placeholder(placeholder.to_string().as_str()))
+        }
+    }
+
+    fn location_placeholder(&mut self, placeholder: &Placeholder) -> Result<String, PlaceholderParseError> {
+        match placeholder.data_type.as_str() {
+            "location::place" => Ok(self.location.place()),
+            "location::street" => Ok(self.location.street()),
+            "location::address" => Ok(self.location.address()),
             _ => Err(PlaceholderParseError::invalid_placeholder(placeholder.to_string().as_str()))
         }
     }
